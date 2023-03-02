a120b23461d170b78d486bc31d6ee696d9ebca20
Samuca47Prog <samuelsimao47@gmail.com>
2023-03-01 22:23:27 -0300

    class 17

--- Django Wednesdays - Codemy/myclub_website/events/views.py
+++ Django Wednesdays - Codemy/myclub_website/events/views.py
@@ -1,17 +1,49 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
-
 import calendar
 from calendar import HTMLCalendar
 from datetime import datetime
-
 from .models import Event, Venue
-
 from .forms import VenueForm, EventForm
-
 from django.http import HttpResponse
-
 import csv
+
+from django.http import FileResponse
+import io
+from reportlab.pdfgen import canvas
+from reportlab.lib.units import inch
+from reportlab.lib.pagesizes import letter
+
+
+
+def venue_pdf(request):
+    buf = io.BytesIO()
+    c = canvas.Canvas(buf, pagesize=letter, bottomup=0)
+
+    textob = c.beginText()
+    textob.setTextOrigin(inch, inch)
+    textob.setFont("Helvetica", 14)
+
+    venues = Venue.objects.all()
+    lines = []
+    for venue in venues:
+         lines.append(venue.name)
+         lines.append(venue.address)
+         lines.append(venue.zip_code)
+         lines.append(venue.phone)
+         lines.append("========")
+
+    for line in lines:
+         textob.textLine(line)
+
+    c.drawText(textob)
+    c.showPage()
+    c.save()
+    buf.seek(0)
+
+    return FileResponse(buf, as_attachment=True, filename='venue.pdf')
+
+
 
 
 
