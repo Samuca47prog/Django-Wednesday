f5616180f72b689ee51356c4797bf4683d37db3e
Samuca47Prog <samuelsimao47@gmail.com>
2023-03-20 23:42:40 -0300

    class 25

--- Django Wednesdays - Codemy/myclub_website/members/views.py
+++ Django Wednesdays - Codemy/myclub_website/members/views.py
@@ -4,6 +4,8 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
 
+from .forms import RegisterUserForm
+
 
 def login_user(request):
     if request.method == "POST":
@@ -35,7 +37,7 @@ def logout_user(request):
 
 def register_user(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = RegisterUserForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data['username']
@@ -47,7 +49,7 @@ def register_user(request):
             messages.success(request, ("Registration successful!"))
             return redirect('home')
     else:
-        form = UserCreationForm()
+        form = RegisterUserForm()
 
     return render(request, "authenticate/register_user.html", {
         'form': form
